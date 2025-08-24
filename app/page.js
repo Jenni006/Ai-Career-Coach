5a6d1d0ce7aaef13f6e767daa817608834928914
Jenni006 <jennifer6kenzo@gmail.com>
2025-08-24 11:38:53 +0530

    initial commit- added exsisting Ai career coach code

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      Hi
+      <br/>
+      <Button>Dark Mode Aware Button</Button>
+    </div>
+  );
+}
