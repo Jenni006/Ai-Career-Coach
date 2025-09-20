bc4a156d38056bf88912231be3257cb334417a2f
Jenni006 <jennifer6kenzo@gmail.com>
2025-09-20 20:01:52 +0530

    turbo

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* other config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // experimental: { turbo: false }, // REMOVE this line
 };
 
 export default nextConfig;
