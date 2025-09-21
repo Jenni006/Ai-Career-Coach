d11ceb68ccc52aa658aaeea218cea697ed470a4b
Jenni006 <jennifer6kenzo@gmail.com>
2025-09-21 11:54:16 +0530

    global css

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,12 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+/** @type {import('postcss').ProcessOptions} */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [
+    tailwindcss,
+    autoprefixer
+  ],
 };
 
 export default config;
